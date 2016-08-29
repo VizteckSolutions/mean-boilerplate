3a8eec76c7984566b81ab2c312396f195fe16962
shahid035 <shahid.ahmad@vizteck.com>
2016-08-29 12:33:39 +0500

    adding jshintrc,bower,gruntfile,package,pm2 files

---
+++ gruntfile.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = function (grunt) {
+    // Project Configuration
+
+
+    //Load NPM tasks
+
+
+    //Making grunt default to force in order not to break the project.
+
+};
